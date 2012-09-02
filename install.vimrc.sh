f9096af1531015e5dde2142fb3f677fc80c9e357
Steve Wattam <stephenwattam@gmail.com>
2012-09-02 22:09:55 +0100

    Added documentation to install script

--- install.vimrc.sh
+++ install.vimrc.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# 
+# Step 1: Check for the user's existing vimrc,
+#         move it to .vimrc.local so it is merged
+#         with the new one (run after it)
+#
+if [ -f "~/.vimrc" ]
+then
+    echo "Moving old vimrc to ~/.vimrc.local"
+    mv ~/.vimrc ~/.vimrc.local
+fi
+
+#
+# Step 2: Symlink ~/.vimrc into this repository
+#         so that it is updated using the usual
+#         git pull
+#
 echo "Symlinking vimrc from `pwd` to ~/.vimrc"
 ln -s "`pwd`/.vimrc" ~/.vimrc
 ln -s "`pwd`/.vimrc" ~/.gvimrc
